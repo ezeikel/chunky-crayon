34af5bc5b4f2732e6203b299e5d14fd01457836f
Ezeikel Pemberton <hi@ezeikel.dev>
2026-01-06 16:25:31 +0400

    fix(mobile): pin metro packages to 0.82.5 to resolve exports mismatch

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -24,6 +24,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       supportsTablet: true,
       bundleIdentifier: "com.chewybytes.chunkycrayon.app",
       infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
         CFBundleURLTypes: [
           {
             CFBundleURLSchemes: [
