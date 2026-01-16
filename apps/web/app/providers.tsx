6352ba38dbff5fde6fab5bb7ec8f4ac26134b5db
Ezeikel Pemberton <hi@ezeikel.dev>
2026-01-16 22:44:36 +0000

    feat: add PlausibleProvider for analytics

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,21 +1,24 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import PlausibleProvider from 'next-plausible';
 import { ColoringContextProvider } from '@/contexts/coloring';
 import { ParentalGateProvider } from '@/components/ParentalGate';
 import UserIdentify from '@/components/UserIdentify/UserIdentify';
 import PixelTracker from '@/components/PixelTracker/PixelTracker';
 
 const Providers = ({ children }: { children: React.ReactNode }) => (
-  <ColoringContextProvider>
-    <SessionProvider>
-      <ParentalGateProvider>
-        <UserIdentify />
-        <PixelTracker />
-        {children}
-      </ParentalGateProvider>
-    </SessionProvider>
-  </ColoringContextProvider>
+  <PlausibleProvider domain="chunkycrayon.com" trackOutboundLinks>
+    <ColoringContextProvider>
+      <SessionProvider>
+        <ParentalGateProvider>
+          <UserIdentify />
+          <PixelTracker />
+          {children}
+        </ParentalGateProvider>
+      </SessionProvider>
+    </ColoringContextProvider>
+  </PlausibleProvider>
 );
 
 export default Providers;
